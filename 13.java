f5ff380b24e7b95562f0c2d5cc0cf9810bf07cab
DK Bhardwaj <99475991+devkrishnabhardwaj@users.noreply.github.com>
2025-04-02 20:36:54 +0530

    roman-to-integer

--- 13.java
+++ 13.java
@@ -1,8 +1,7 @@
 // Problem: https://leetcode.com/problems/roman-to-integer/submissions/867440410/
 class Solution {
-public:
-    int val(char c){
-        int a;
+    public static int value(char c){
+        int a=0;
         switch(c){
             case 'I' : a=1; break;
             case 'V' : a=5; break;
@@ -14,17 +13,17 @@ public:
         }
         return a;
     }
-    int romanToInt(string s) {
-        int n= s.length()-1,value = val(s.at(n));
-        n--;
-        while(n+1){
-            
-            if(val(s.at(n+1))>val(s.at(n)))
-            value = value - val(s.at(n));
-            else
-            value += val(s.at(n));
-            n--;
+    public int romanToInt(String s) {    
+        int num=value(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--){
+            int temp = value(s.charAt(i));
+            if(value(s.charAt(i+1))<=temp){
+                num = num + temp;
+            }
+            else{
+                num = num - temp;
+            }
         }
-        return value;
+        return num;
     }
-};
+}
